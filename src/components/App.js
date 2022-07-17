93c6278d8aee659f2e67091110a44580c373e91d
dchoure1 <dchoure1@gmail.com>
2022-07-17 13:00:21 +0530

    wekend

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,26 @@
 import React, { useState } from 'react'
 import '../styles/App.css';
 import star from '../star.png'
+
 const App = () => {
+  let [size, setSize] = useState(0);
+  let style = {
+    height: 300 + size + "px",
+    width: 300 + size + "px"
+  }
+
+  const handleClick = ()=>{
+    size = size + 2;
+    setSize(size);
+  }
+
+
   return (
     <div id="main">
-      <img src={star}  height="300px" width="300px" />
-      <img src={star}  height="300px" width="300px" />
+      <img src={star} style={style}  height="300px" width="300px" onClick={handleClick}/>
     </div>
-
-
   )
 }
+
 
 export default App;
